bff418ed64ff8922b41b3f8cb0722c5739109993
jkominek <kominek@gmail.com>
1999-02-06 17:15:38 +0000

    Lots of stuff cleaned up so that perl -w doesn't complain. Beginnings of support for inter-server communication in pircd, Connection.pm  and Server.pm. doesn't work yet.

--- Connection.pm
+++ Connection.pm
@@ -2,7 +2,7 @@
 # 
 # Connection.pm
 # Created: Tue Sep 15 14:26:26 1998 by jay.kominek@colorado.edu
-# Revised: Wed Dec  2 21:52:19 1998 by jay.kominek@colorado.edu
+# Revised: Sat Feb  6 10:07:43 1999 by jay.kominek@colorado.edu
 # Copyright 1998 Jay F. Kominek (jay.kominek@colorado.edu)
 #
 # This program is free software; you can redistribute it and/or modify it
@@ -35,12 +35,12 @@ sub new {
   my $class = ref($proto) || $proto;
   my $this  = { };
 
-  $this->{socket}      = shift;
+  $this->{'socket'}      = shift;
   $this->{outbuffer}   = shift;
   $this->{server}      = shift;
   $this->{connected} = $this->{last_active} = time();
 
-  my($port,$iaddr)     = sockaddr_in(getpeername($this->{socket}));
+  my($port,$iaddr)     = sockaddr_in(getpeername($this->{'socket'}));
   $this->{host}        = gethostbyaddr($iaddr,AF_INET) || inet_ntoa($iaddr);
   $this->{host_ip}     = inet_ntoa($iaddr);
 
@@ -51,7 +51,7 @@ sub new {
 # Get our socket
 sub socket {
   my $this = shift;
-  return $this->{socket};
+  return $this->{'socket'};
 }
 
 # Handle some data
@@ -112,6 +112,13 @@ sub handle {
       my($command,$servername,$distance,$timea,$timeb,$proto,$description) =
 	split(/\s+/,$line);
       $description =~ s/^://;
+      $this->{servername}  = $servername;
+      $this->{proto}       = $proto;
+      $this->{distance}    = $distance;
+      $this->{description} = $description;
+      if($this->{password}) {
+	$this->{ready} = 1;
+      }
       last SWITCH;
     }
     if($line =~ /^PASS/i) {
@@ -119,6 +126,9 @@ sub handle {
       $password =~ s/^://;
       # We don't actually do anything with the password yet
       $this->{password} = $password;
+      if($this->{servername}) {
+	$this->{ready} = 1;
+      }
       last SWITCH;
     }
   }
@@ -158,13 +168,16 @@ sub finalize {
     # become a user.
     my $user = User->new($this);
 
-    # This is for local users only, since we've got
-    # a socket directly to them.
-    $user->setupaslocal($this->{socket},$this->{outbuffer});
+    # We used to have to tell User objects that they were local, but
+    # now they figure it out for themselves based on the fact that they
+    # have sockets. Pretty clever of them, eh?
 
     return $user;
   } elsif($this->{servername}) {
     # We're trying to finalize as an IRC server
+    my $server = Server->new($this);
+
+    return $server;
   }
   return undef;
 }
@@ -173,7 +186,7 @@ sub senddata {
   my $this = shift;
   my $data = shift;
 
-  $this->{outbuffer}->{$this->{socket}} .= join('',$data);
+  $this->{outbuffer}->{$this->{'socket'}} .= join('',$data);
 }
 
 1;
