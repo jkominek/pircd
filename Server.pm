479218baddb37221346b387feaf55b646ee06a3b
jkominek <jkominek>
1999-02-08 06:02:26 +0000

    Further changes to facilitate functioning interserver communication.  * addcommand method was made to work. (lots of shifts didn't)  * server method added. parent method fixed.  * "Received unknown command string" line fixed to not crash by calling nick    method  * When bursting, server tree is (theoretically) properly descended and spewed    via the new spewchildren method. Channel data is partially spewed, user    information should be fully spewed.

--- Server.pm
+++ Server.pm
@@ -2,7 +2,7 @@
 # 
 # Server.pm
 # Created: Tue Sep 15 13:55:23 1998 by jay.kominek@colorado.edu
-# Revised: Sat Feb  6 10:11:13 1999 by jay.kominek@colorado.edu
+# Revised: Sun Feb  7 22:52:53 1999 by jay.kominek@colorado.edu
 # Copyright 1998 Jay F. Kominek (jay.kominek@colorado.edu)
 # 
 # This program is free software; you can redistribute it and/or modify it
@@ -41,14 +41,12 @@ sub new {
   my $this  = { };
   my $connection = shift;
 
-  $this->{name}        = $connection->{name};
+  $this->{name}        = $connection->{servername};
   $this->{description} = $connection->{description};
   $this->{distance}    = $connection->{distance};
   $this->{proto}       = $connection->{proto};
   $this->{server}      = $connection->{server};
   $this->{last_active} = time();
-
-  print $this->{name}." has connected using ".$this->{proto}.", it is at a distance of ".$this->{distance}."\n";
 
   bless($this, $class);
   if(defined($connection->{'socket'})) {
@@ -61,8 +59,110 @@ sub new {
 }
 
 sub setupaslocal {
+  my $this = shift;
 
+  $this->addcommand('PING',    \&handle_ping);
 
+  # Channel membership
+  $this->addcommand('JOIN',    \&handle_join);
+  $this->addcommand('PART',    \&handle_part);
+  $this->addcommand('INVITE',  \&handle_invite);
+  $this->addcommand('KICK',    \&handle_kick);
+  # Channel status
+  $this->addcommand('TOPIC',   \&handle_topic);
+  $this->addcommand('MODE',    \&handle_mode);
+
+  # User presence
+  $this->addcommand('NICK',    \&handle_nick);
+  $this->addcommand('KILL',    \&handle_kill);
+  $this->addcommand('QUIT',    \&handle_quit);
+  # User status
+  $this->addcommand('AWAY',    \&handle_away);
+
+  # Server presence
+  $this->addcommand('SERVER',  \&handle_server);
+  $this->addcommand('SQUIT',   \&handle_squit);
+
+  # Communication
+  $this->addcommand('PRIVMSG', \&handle_privmsg);
+  $this->addcommand('NOTICE',  \&handle_notice);
+
+  # Burst commands
+  $this->addcommand('BU',      \&handle_burstuser);
+  $this->addcommand('BC',      \&handle_burstchannel);
+
+  # lookup and send password
+  $this->senddata("PASS :password\r\n");
+
+  ###################################################################
+  # Start dumping the state of the entire network at the new server #
+  my $server = $this->server;
+
+  # Dump the server tree
+
+  # This server has to be sent in the special fashion. All other
+  # servers are spewed using the recursive method 'spewchildren'
+  $this->senddata(join(' ',
+		       $server->name,
+		       1,
+		       0,    # timestamp a
+		       time, # timestamp b
+		       "Px1",
+		       ":".$server->description)."\r\n");
+  my $childserver;
+  foreach $childserver ($server->children()) {
+    $this->spewchildren($childserver);
+  }
+
+  # Dump the users
+  # For big networks, it would be a very good idea to generate all the
+  # data that needs to be sent at one time, use Compress::Zlib on that
+  # data and then spew it binaryily.
+
+  # As it is, we will merely use the special burst user command
+  my $user;
+  foreach $user (values(%{Utils::users()})) {
+    $this->senddata(join(' ',
+			 ":".$user->server->name,
+			 "BU", # Burst User. Only place of generation.
+			 $user->nick,
+			 $user->username,
+			 $user->host,
+			 $user->genmodestr,
+			 $user->ircname)."\r\n");
+  }
+
+  # Burst channel information
+  my $channel;
+  foreach $channel (values(%{Utils::channels()})) {
+    # :server.name BC #channel creationtime modestr modeargs
+    #  nick @opnick +@opvoicenick +voicenick nick +voicenick2 @opnick2
+    $this->senddata(join(' ',
+			 ":".$this->parent->name,
+			 "BC", # Burst Channel. Only place of generation.
+			 $channel->name,
+			 $channel->creation,
+			 map { $_->nick } $channel->users()
+			)."\r\n"
+		   );
+  }
+}
+
+sub spewchildren {
+  my $this = shift;
+  my $server = shift;
+  $this->senddata(join(' ',
+		       ":".$this->server->name,
+		       "SERVER",
+		       $server->name,
+		       $server->distance+1,
+		       0,    # timestamp a
+		       time, # timestamp b
+		       "Px1",
+		       ":".$server->description)."\r\n");
+  foreach($server->children()) {
+    $this->spewchildren($_);
+  }
 }
 
 sub handle {
@@ -71,13 +171,13 @@ sub handle {
 
   my $line;
   foreach $line (split(/\x00/,$rawline)) {
-
     $line =~ s/\s+$//;
 
     $this->{last_active} = time();
     delete($this->{ping_waiting});
 
-    my $command = "NULL";
+    $line =~ /^\S+ (\S+) .+$/;
+    my $command = $1;
 
     # Parsing stuff from a server is a bit more complicated than parsing
     # for a user.
@@ -85,10 +185,40 @@ sub handle {
       &{$this->{commands}->{$command}}($this,$line);
     } else {
       if($line =~ /[\w\d\s]/) {
-	print "Received unknown command string \"$line\" from ".$this->nick."\n";
+	print "Received unknown command string \"$line\" from ".$this->name."\n";
       }
     }
   }
+}
+
+sub handle_ping {
+  my $this = shift;
+  print "handle_ping called\n";
+}
+
+sub handle_nick {
+  my $this = shift;
+
+}
+
+sub handle_kill {
+  my $this = shift;
+
+}
+
+sub handle_quit {
+  my $this = shift;
+
+}
+
+sub handle_away {
+  my $this = shift;
+
+}
+
+sub squit {
+  my $this = shift;
+  
 }
 
 #####################################################################
@@ -170,7 +300,12 @@ sub ping_in_air {
 # has to be sent through to be routed properly.)
 sub parent {
   my $this = shift;
-  return $this->{'parent'};
+  return $this->{'server'};
+}
+
+sub server {
+  my $this = shift;
+  return $this->{'server'};
 }
 
 # Returns the number of hops to reach the local
@@ -236,7 +371,10 @@ sub removechildserver {
 
 # Add a command to the hash of commandname->subroutine refs
 sub addcommand {
-  shift()->{'commands'}->{shift()} = shift();
+  my $this    = shift;
+  my $command = shift;
+  my $subref  = shift;
+  $this->{'commands'}->{$command} = $subref;
 }
 
 sub senddata {
